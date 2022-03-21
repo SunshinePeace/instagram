78eb18ba045b3d92078e1ec42c12c7ee8907f878
SunshinePeace <okwan48@gmail.com>
2022-03-21 20:26:37 +0800

    final

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -11,7 +11,7 @@ const MiniProfile:React.FC = () => {
             alt="profile pic" className="rounded-full border p-[2px] w-16 h-16"/>
 
             <div className="flex-1 mx-4">
-                <h2 className="font-bold">{session?.user.username}</h2>
+                <h2 className="font-bold">{session?.user.username?session?.user.username:undefined}</h2>
                 <h3 className=" text-sm text-gray-400">Welcome to IG!!!</h3>
             </div>
             <button className="text-blue-400 text-sm font-semibold" onClick={() => signOut()}>Sign Out</button>
