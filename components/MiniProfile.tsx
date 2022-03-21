5784d39b55514b15a85f87af0aadb8ba878931fe
SunshinePeace <okwan48@gmail.com>
2022-03-21 21:54:01 +0800

    final

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -4,7 +4,6 @@ import { signOut, useSession } from "next-auth/react"
 const MiniProfile:React.FC = () => {
 
     const { data: session } = useSession()
-    const username:(String | null| undefined) = session?.user.username;
 
     return (
         <div className="flex items-center justify-between mt-14 ml-10">
@@ -12,7 +11,7 @@ const MiniProfile:React.FC = () => {
             alt="profile pic" className="rounded-full border p-[2px] w-16 h-16"/>
 
             <div className="flex-1 mx-4">
-                <h2 className="font-bold">{username}</h2>
+                <h2 className="font-bold">{session?.user.username ?? undefined}</h2>
                 <h3 className=" text-sm text-gray-400">Welcome to IG!!!</h3>
             </div>
             <button className="text-blue-400 text-sm font-semibold" onClick={() => signOut()}>Sign Out</button>
